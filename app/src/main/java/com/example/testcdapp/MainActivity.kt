6307e40b3ea76e6ff9a6bb75ece36c4327a9431b
manh <manh@nimblehq.co>
2021-04-05 17:46:11 +0700

    sdf

--- app/src/main/java/com/example/testcdapp/MainActivity.kt
+++ app/src/main/java/com/example/testcdapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.testcdapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        Log.d("")
         setContentView(R.layout.activity_main)
     }
 }
